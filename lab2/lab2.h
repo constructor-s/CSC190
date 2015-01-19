620986251bf6ab4cf7ac0186ab46741877b6c558
shirj <shirunjie@126.com>
2015-01-19 16:46:07 -0500

    change lab2.h to new version

--- lab2/lab2.h
+++ lab2/lab2.h
@@ -1,14 +1,13 @@
 /*
  * Include necessary libraries (stdio.h and stdlib.h)
  */
-#include <stdio.h>
-#include <stdlib.h>
+
 #define MATSIZE		5
 
-void freeMatrix(int rows, int ** matrix);
+void freeMatrix(int n, int ** matrix);
 void printMatrix(int n, int ** A);
 int readMatrixSize(char * filename);
 int **readMatrix(char * filename);
-void initMatrix(int ***matrix, int size);
+void initMatrix(int ***mat, int n);
 int **sum(int ** A, int ** B, int x1, int y1, int x2, int y2, int n);
 int **sub(int ** A, int ** B, int x1, int y1, int x2, int y2, int n);
